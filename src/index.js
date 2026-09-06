87d620363869f68fd065069137954a14861aa50a
chingta <rafirezkchingta@gmail.com>
2026-09-06 11:24:13 +0700

    Fix Express deployment

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import customerRoutes from "./routes/customerRoutes.js";
 dotenv.config();
 
 const app = express();
+
 app.use(express.json());
 
 app.use("/api/categories", categoryRoutes);
@@ -14,6 +15,11 @@ app.use("/api/products", productRoutes);
 app.use("/api/customers", customerRoutes);
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
-});
+
+if (process.env.VERCEL !== "1") {
+  app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+  });
+}
+
+export default app;
